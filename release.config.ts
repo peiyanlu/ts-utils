6a158d22b7880f23c2e6afa22d23af1b88b8fcea
YanluPei <peiyanlu@vip.qq.com>
2026-04-09 17:29:58 +0800

    config: 升级 tsconfig

--- release.config.ts
+++ release.config.ts
@@ -1,14 +1,4 @@
 import { defineConfig } from '@peiyanlu/release'
 
 
-export default defineConfig({
-  toTag(pkg: string, version: string): string {
-    return `${ version }`
-  },
-  git: {
-    commitMessage: 'chore(release): v${tag}',
-  },
-  github: {
-    releaseName: 'v${tag}',
-  },
-})
+export default defineConfig({})
